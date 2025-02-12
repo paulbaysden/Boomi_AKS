eaa2b5f18e1d2e3472f645a5c6f5c1090a8a9639
paulbaysden <86688645+paulbaysden@users.noreply.github.com>
2025-02-12 17:38:25 -0500

    Update main.tf

--- terraform/scripts/main.tf
+++ terraform/scripts/main.tf
@@ -170,6 +170,7 @@ resource "azurerm_netapp_volume" "netapp_volume" {
   export_policy_rule {
     rule_index       = 1
     allowed_clients  = ["0.0.0.0/0"]
+    protocols_enabled = ["NFSv4.1"]
     unix_read_only   = false
     unix_read_write  = true
   }
